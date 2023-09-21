401f5571adb467927ded7ad4e43de9e6fe00f6c0
agostinho f s neto silva netto <agostneto6@gmail.com>
2023-09-21 15:03:02 -0300

    criando create e update

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,20 +1,36 @@
-<form action="{{ route('posts.store') }}" method="POST">
-    @csrf
-    <div class="form-group">
-        <label for="title">Título</label>
-        <input type="text" name="title" id="title" class="form-control">
-    </div>
-    <div class="form-group">
-        <label for="slug">Slug</label>
-        <input type="text" name="slug" id="slug" class="form-control">
-    </div>
-    <div class="form-group">
-        <label for="content">Conteúdo</label>
-        <textarea name="content" id="content" class="form-control" rows="4"></textarea>
-    </div>
-    <div class="form-group">
-        <label for="published">Publicado</label>
-        <input type="checkbox" name="published" value="published">
-    </div>
-    <button type="submit" class="btn btn-primary">Criar Post</button>
-</form>
+<x-app-layout>
+    <form action="{{ route('posts.store') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        <div class="form-group">
+            <label for="title">Título</label>
+            <input type="text" name="title" id="title" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="slug">Slug</label>
+            <input type="text" name="slug" id="slug" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="content">Conteúdo</label>
+            <textarea name="content" id="content" class="form-control" rows="4"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="published">Publicado</label>
+            <input type="checkbox" name="published" value="published">
+        </div>
+        <button type="submit" class="btn btn-primary">Criar Post</button>
+    </form>
+</x-app-layout>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,45 +1,70 @@
-<x-app-layout>
-    <div class="container mx-auto flex flex-wrap py-6">
-        <!-- Posts Section -->
-        <div>
-            <a href="{{ route('posts.create') }}" class="btn btn-primary">Criar Post</a>
-        </div>
-        <br>
-        <section class="w-full md:w-2/3  px-3">
-            <div class=" flex flex-col items-center">
-                @foreach ($posts as $post)
-                    <x-post-list-item :post="$post" />
-
-
-                    <div>
-                        <a href="{{ url('posts/' . $post->id) }}" title="Ver Posts">
-                            <button class="btn btn-info btn-sm">
-                                <i class="fa fa-eye" aria-hidden="true">Detalhes</i>
-                            </button>
-                        </a>
-                    </div>
-
-                    <div>
-                        <a href="{{ url('posts/edit/' . $post->id) }}" title="Ver Posts">
-                            <button class="btn btn-info btn-sm">
-                                <i class="fa fa-eye" aria-hidden="true">Editar</i>
-                            </button>
-                        </a>
-                    </div>
-
-                    <div>
-                        <a href="{{ url('posts/destroy/' . $post->id) }}" title="Ver Posts">
-                            <button class="btn btn-info btn-sm">
-                                <i class="fa fa-eye" aria-hidden="true">Deletar</i>
-                            </button>
-                        </a>
-                    </div>
-
-                @endforeach
-            </div>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Crud de Posts</title>
+</head>
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <div class="container">
+        <div class="row">
             <div>
-                {{ $posts->links('pagination::tailwind') }}
+                <a href="{{ route('posts.create') }}" class="btn btn-primary">Criar Post</a>
             </div>
-        </section>
+            <div class="container mx-auto flex flex-wrap py-6">
+                <section class="w-full md:w-2/3  px-3">
+                    <div class=" flex flex-col items-center">
+
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">Slug</th>
+                                    <th scope="col">Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($posts as $post)
+                                    <tr>
+                                        <th scope="row">#</th>
+                                        <td>{{ $post->id }}</td>
+                                        <td>{{ $post->title }}</td>
+                                        <td>{{ $post->slug }}</td>
+                                        <td>
+                                        <a href="{{ url('posts/' . $post->id) }}" title="Ver Posts">
+                                            <button class="btn btn-info btn-sm">
+                                                <i class="fa fa-eye" aria-hidden="true">Detalhes</i>
+                                            </button>
+                                        </a>
+                                        <a href="{{ url('posts/edit/' . $post->id) }}" title="Ver Posts">
+                                            <button class="btn btn-info btn-sm">
+                                                <i class="fa fa-eye" aria-hidden="true">Editar</i>
+                                            </button>
+                                        </a>
+                                        <a href="{{ url('posts/destroy/' . $post->id) }}" title="Ver Posts">
+                                            <button class="btn btn-info btn-sm">
+                                                <i class="fa fa-eye" aria-hidden="true">Deletar</i>
+                                            </button>
+                                        </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+
+                        {{ $posts->links('pagination::tailwind') }}
+                    </div>
+                    <div>
+                    </div>
+                </section>
+            </div>
+        </div>
     </div>
-</x-app-layout>
+</body>
+</html>
